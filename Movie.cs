b683697beb65536bcb5573eab9c4ee1f40518645
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 21:50:12 -0500

    Task Done

---
+++ Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersAndSettersPractice
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; 
+
+
+        public Movie(string aTitle, String aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating // properties
+        {
+            get { return rating; }
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NC-17") 
+
+                {
+                    rating = value;
+                }
+                 else
+                {
+                    rating = "NR";
+                }
+
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettersAndSettersPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie boost = new Movie("Boost","Koko", "DOG");
+            Movie hoops = new Movie( "Hoops","Tony", "PG");
+            //G,PG,PG-13,R,NR
+
+            Console.WriteLine(boost.Rating);// set to private in movie class
+
+        }
+    }
+}
